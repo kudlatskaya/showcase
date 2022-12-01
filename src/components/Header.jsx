2362cd0573c767212e6ed828c3862d070fbb218d
Marina_Kudlatskaya <m.kudlatskaya@gmail.com>
2022-12-01 14:32:22 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
             <div className="nav-wrapper">
             <a href="#" className="brand-logo">Showcase</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
+                <li><a href="https://github.com/kudlatskaya/showcase" target='_blank' rel="noreferrer">Repo</a></li>
             </ul>
             </div>
         </nav>
